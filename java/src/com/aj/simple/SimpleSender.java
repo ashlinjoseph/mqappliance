e726ae18183be63f02ceaadc16789142ea0de131
Ashlin Joseph <a5hlin@yahoo.com>
2021-02-15 12:57:05 +0000

    Adding a simple sender app

--- java/src/com/aj/simple/SimpleSender.java
+++ java/src/com/aj/simple/SimpleSender.java
@@ -1,4 +1,4 @@
-package com.aj.ssl;
+package com.aj.simple;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -6,11 +6,9 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-//import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
 import javax.jms.JMSException;
 import javax.jms.JMSProducer;
-//import javax.jms.Message;
 import javax.jms.Queue;
 import javax.jms.TextMessage;
 
@@ -18,17 +16,13 @@ import com.ibm.mq.jms.MQQueueConnectionFactory;
 import com.ibm.msg.client.wmq.WMQConstants;
 
 /**
- * This app can be used for TLS/SSL Two Way mutual authentication
- * First you need a keystore and truststore at the queue manager. 
- * Create a certificate, get it signed and export the public part out. 
- * Create a keystore and truststore at the Client App (.jks). 
- * Create a certificate, get it signed and export the public part out.
- * Add eachothers pub key to the trust store. 
- * At the channel, set a cipher spec that matches the cipher suite set 
- * in the JMS app; set SSL Auth required and you're done.   
+ * SimpleSender.java is a simple JMS sender app that can connect to a
+ * queue in a queue manager using username and password as security
+ * and send a simple message. 
  *  
  * */
-public class SenderWithSSL {
+
+public class SimpleSender {
 
 	public static void main(String[] args) {
 		
@@ -42,14 +36,14 @@ public class SenderWithSSL {
 		String senderQ = System.getProperty("senderQ");
 		String APP_USER = System.getProperty("user"); // User name that application uses to connect to MQ
 		String APP_PASSWORD = System.getProperty("pwd"); // Password that the application uses to connect to MQ
-		String cipherSuite = System.getProperty("cipherSuite"); // Cipher Suite needs to match the Cipher spec at the Queue Manager
 		String logfile = System.getProperty("logFile");
+
 		try {
 			fileHandler = new FileHandler(logfile);  
 	        logger.addHandler(fileHandler);
 	        SimpleFormatter formatter = new SimpleFormatter();  
 	        fileHandler.setFormatter(formatter);
-			
+	        
 			MQQueueConnectionFactory cf = new MQQueueConnectionFactory();
 			cf.setTransportType(1);
 			cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
@@ -60,14 +54,13 @@ public class SenderWithSSL {
 			cf.setStringProperty(WMQConstants.USERID, APP_USER);
 			cf.setStringProperty(WMQConstants.PASSWORD, APP_PASSWORD);
 			cf.setBooleanProperty(WMQConstants.USER_AUTHENTICATION_MQCSP, true);
-			cf.setSSLCipherSuite(cipherSuite);
 
 			logger.info("Connecting to "+qm+" at "+hostname+":"+port + " via channel: " + channel);
 			JMSContext context = cf.createContext();
-
+			
 			JMSProducer prod = context.createProducer();
 			Queue senderQueue = context.createQueue(senderQ);
-			
+
 			TextMessage textMessage = context.createTextMessage();
 			textMessage.setText("Ashlin's message is " + UUID.randomUUID().toString());
 			prod.send(senderQueue, textMessage);
@@ -78,10 +71,10 @@ public class SenderWithSSL {
 			
 		} catch (JMSException e) {
 			e.printStackTrace();
-		} catch (SecurityException e) {  
-		        e.printStackTrace();  
-	    } catch (IOException e) {  
-	        e.printStackTrace();  
-	    } 
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
