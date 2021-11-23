535060218e76d1823aa0f57fc4e0c3efc9906b8a
Ashlin Joseph <aj@Ashlins-MacBook-Pro.local>
2021-11-23 19:08:40 +0200

    rename and json response

--- python/displayqmgrstatus.py
+++ python/displayqmgrstatus.py
@@ -6,5 +6,5 @@ password='mqadmin'
 url="https://18.130.221.119:9443/ibmmq/rest/v1/admin/qmgr"
 
 response = requests.get(url=url,auth=(user,password),verify=False)
-print('Response code',response.text)
+print('Response code',response.json())
 print ('Testing python by AJ')
