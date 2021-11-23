34b8bd81a43424accaf4882ca3e656973ea7e6bd
Ashlin Joseph <aj@Ashlins-MacBook-Pro.local>
2021-11-23 19:04:50 +0200

    Working basic

--- python/test.py
+++ python/test.py
@@ -1,2 +1,10 @@
-print ('Testing python by AJ')
+import requests
+from requests.auth import HTTPBasicAuth
 
+user='mqadmin'
+password='mqadmin'
+url="https://18.130.221.119:9443/ibmmq/rest/v1/admin/qmgr"
+
+response = requests.get(url=url,auth=(user,password),verify=False)
+print('Response code',response.text)
+print ('Testing python by AJ')
